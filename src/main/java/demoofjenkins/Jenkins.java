14e2f648caf612fe3eaaf9d51b674e71965d6c8a
Dingu VM <dinakaranvm@gmail.com>
2018-02-10 12:58:21 +0100

    Try

---
+++ src/main/java/demoofjenkins/Jenkins.java
@@ -0,0 +1,37 @@
+package demoofjenkins;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Jenkins {
+
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		
+		try{
+			driver.get("https://www.facebook.com/");
+			driver.manage().window().maximize();
+			//Thread.sleep(10000);
+			//driver.findElementByXPath("//input[@id='email']").sendKeys("dinakaranvm@gmail.com");
+			System.out.println(driver.getTitle());
+			driver.quit();
+			
+			/*driver.findElementByXPath("//input[contains(@id,'pass')]").sendKeys("Myfacebook");
+			driver.findElementByXPath("//input[contains(@value,'Log In')]").click();*/
+			
+			
+			
+		  }
+		catch (Exception e){
+			
+			System.out.println(" Exception here");
+		}
+		
+	}
+
+}
