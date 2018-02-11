b6ef2d4eef1ad8fdf7472afe9143ce66cd2135d3
Dingu VM <dinakaranvm@gmail.com>
2018-02-11 10:38:34 +0100

    Update of POM

--- src/main/java/demoofjenkins/Jenkins.java
+++ src/main/java/demoofjenkins/Jenkins.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Jenkins {
 
-	public static void main(String[] args)  {
+	@Test
+	public static void main()  {
 		// TODO Auto-generated method stub
 		
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
